62b94f39ae05591acb07131ba47b2b196bbc0a1f
AntoninaTikhonova <antonina.pimenova@mail.ru>
2022-10-27 22:39:05 +0700

    ended homework_9

--- homework_9/script.js
+++ homework_9/script.js
@@ -9,32 +9,29 @@ resultButton.addEventListener('click', (event) => {
     const secondValue = document.getElementById('second-value').value;
     const operator = document.getElementById('operator').value;
 
-    switch (operator) {
-        case '': result = 'Не выбрана операция'; break;
-        case '+': result = Number(firstValue) + Number(secondValue); break;
-        case '-': result = Number(firstValue) - Number(secondValue); break;
-        case '*': result = Number(firstValue ) * Number(secondValue); break;
-        case '/': result = Number(firstValue ) / Number(secondValue); break;
-        default: result = 'Программа не поддерживает такую операцию'; break;
-    }
-
     if (firstValue === '') {
         result = 'Первое число не указано';
-    }
-
-    if (secondValue === '') {
-        result = 'Второе число не указано';
-    }
-
-    if (isNaN(firstValue) || isNaN(secondValue)) {
-        result = 'Число указано не верно'
-    }
-
-    if (secondValue === 0 && operator === '/') {
-        result = 'Операция не корректна';
-    }
-
+        } 
+        else if (secondValue === '') {
+            result = 'Второе число не указано';
+        }
+        else if (isNaN(firstValue) || isNaN(secondValue)) {
+            result = 'Число указано не верно';
+        }
+        else if (secondValue === '0' && operator === '/') {
+            result = 'Операция не корректна';
+        }
+        else {
+            switch (operator) {
+                case '': result = 'Не выбрана операция'; break;
+                case '+': result = Number(firstValue) + Number(secondValue); break;
+                case '-': result = Number(firstValue) - Number(secondValue); break;
+                case '*': result = Number(firstValue ) * Number(secondValue); break;
+                case '/': result = Number(firstValue ) / Number(secondValue); break;
+                default: result = 'Программа не поддерживает такую операцию'; break;
+            }
+        }
     console.log(result);
-    document.getElementById('result').innerText = result;
+    document.getElementById('result').textContent = result;
 }
 )
